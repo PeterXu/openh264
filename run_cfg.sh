9bad78b32d69a64690a71ae7a0c4c94affe8b9ab
PeterXu <peter@uskee.org>
2016-08-12 10:33:58 +0800

    update

--- run_cfg.sh
+++ run_cfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ARCH=${1:-arm}
-make OS=android ARCH=arm NDKROOT=$ANDROID_NDK TARGET=android-22 PREFIX=out install
+make OS=android ARCH=${ARCH} NDKROOT=$ANDROID_NDK TARGET=android-15 PREFIX=out install
 #rm -f out/lib/libopenh264.a
 
 exit 0
