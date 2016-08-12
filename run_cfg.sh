863bfd42dbd72e94e8ca207c52136324e94b97cc
PeterXu <peter@uskee.org>
2016-08-12 11:04:16 +0800

    refine

--- run_cfg.sh
+++ run_cfg.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ "$1" = "clean" ]; then
+    make clean
+    find . -name *.o -exec rm -f {} \;
+    exit 0
+fi
+
+# arm/arm64
 ARCH=${1:-arm}
-make OS=android ARCH=${ARCH} NDKROOT=$ANDROID_NDK TARGET=android-15 PREFIX=out install
+make OS=android ARCH=${ARCH} NDKROOT=$ANDROID_NDK TARGET=android-15 NDKLEVEL=21 PREFIX=out/$ARCH install
 #rm -f out/lib/libopenh264.a
 
 exit 0
